f0235b5ef6298c63213f77cd12fb0ed242f7f3c8
Gareth Funk <gareth.funk94@gmail.com>
2019-12-27 14:32:27 +0000

    Fixed a bug with consecutive email sends. Disabled gvoice logout Now supports list of recipients

--- alert.py
+++ alert.py
@@ -12,26 +12,32 @@ Gareth Funk 2019
 
 class Alert:
     def __init__(self):
-        self.__smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
-        self.__smtp_server.ehlo()
-        self.__smtp_server.starttls()
-        self.__smtp_server.login(smtp_username, smtp_password)
         self.__gvoice = Voice()
         #self.__gvoice.login(gvoice_username, gvoice_password)
         return
 
     def __del__(self):
-        self.__smtp_server.quit()
-        self.__gvoice.logout()
+        #self.__gvoice.logout()
         return
 
-    def Email(self, email):
+    def Email(self, emails):
+        if type(emails) is str:
+            email = emails
+        elif type(emails) is list:
+            email = ", ".join(emails)
+        else:
+            raise TypeError("Unsupported argument type: " + str(type(emails)))
         msg = EmailMessage()
         msg.set_content("Test")
         msg["Subject"] = "Test Subject"
         msg["From"] = smtp_from
         msg["To"] = email
-        self.__smtp_server.send_message(msg)
+        smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+        smtp_server.ehlo()
+        smtp_server.starttls()
+        smtp_server.login(smtp_username, smtp_password)
+        smtp_server.send_message(msg)
+        smtp_server.quit()
         return
 
     def Sms(self, number):
@@ -41,4 +47,5 @@ class Alert:
 if __name__ == "__main__":
     addr = smtp_from
     a = Alert()
+    a.Email(addr)
     a.Email(addr)
